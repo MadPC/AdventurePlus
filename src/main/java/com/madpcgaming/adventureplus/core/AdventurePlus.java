924840acde83a85506e9304ff766d79c7074f714
Daniel Sapelli <daniel.sapelli@gmail.com>
2014-01-29 03:13:00 -0500

    Removed Ore Generation for now. Should fix start-up crash.

--- src/main/java/com/madpcgaming/adventureplus/core/AdventurePlus.java
+++ src/main/java/com/madpcgaming/adventureplus/core/AdventurePlus.java
@@ -3,13 +3,11 @@ import com.madpcgaming.adventureplus.blocks.ModBlocks;
 import com.madpcgaming.adventureplus.items.ModItems;
 import com.madpcgaming.adventureplus.lib.Reference;
 
-import cpw.mods.fml.common.IWorldGenerator;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 
@@ -18,7 +16,6 @@ public class AdventurePlus
 	@Instance("Adv+")
 	public static AdventurePlus instance;
 
-	private static final IWorldGenerator Eventmanager = null;
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
@@ -30,9 +27,6 @@ public class AdventurePlus
     @EventHandler
     public void load(FMLInitializationEvent event)
     {
-         
-         
-    
-          GameRegistry.registerWorldGenerator(Eventmanager, 0);    
+  
     }
 }
