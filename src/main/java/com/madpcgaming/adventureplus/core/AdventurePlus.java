a738eeebcd921abfd4b0024ffb7adc5d5a107a7a
Daniel Sapelli <daniel.sapelli@gmail.com>
2014-02-02 03:25:47 -0500

    Added Actual World Generation. Fixed a naming glitch.

--- src/main/java/com/madpcgaming/adventureplus/core/AdventurePlus.java
+++ src/main/java/com/madpcgaming/adventureplus/core/AdventurePlus.java
@@ -3,12 +3,14 @@ import com.madpcgaming.adventureplus.blocks.ModBlocks;
 import com.madpcgaming.adventureplus.items.ModItems;
 import com.madpcgaming.adventureplus.lib.AdventureConfig;
 import com.madpcgaming.adventureplus.lib.Reference;
+import com.madpcgaming.adventureplus.world.AdvBaseWorldGeneration;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 
@@ -24,6 +26,7 @@ public class AdventurePlus
 		AdventureConfig.initProps(event.getModConfigurationDirectory());
 		ModBlocks.init();
 		ModItems.init();
+		GameRegistry.registerWorldGenerator(new AdvBaseWorldGeneration(), 0);
 	}
    
     @EventHandler
--- src/main/java/com/madpcgaming/adventureplus/lib/Reference.java
+++ src/main/java/com/madpcgaming/adventureplus/lib/Reference.java
@@ -7,7 +7,7 @@ package com.madpcgaming.adventureplus.lib;
 public class Reference
 {
 	public static final String	MOD_ID				= "adventureplus";
-	public static final String	MOD_NAME			= "Adventur+";
+	public static final String	MOD_NAME			= "Adventure+";
 	public static final String	VERSION				= "pre-release-0.0.1";
 	public static final String	CHANNEL_NAME		= MOD_ID;
 }
